17430e746dd6a2916822bbfc522148ec7f9a730d
Olga <olg3001@yandex.ru>
2022-12-26 18:50:04 +0300

    DZ20

---
+++ DZ20.py
@@ -0,0 +1,58 @@
+# Д, К, Л, М, П, У – 2 очка;
+# Б, Г, Ё, Ь, Я – 3 очка;
+# Й, Ы – 4 очка;
+# Ж, З, Х, Ц, Ч – 5 очков;
+# Ш, Э, Ю – 8 очков;
+# Ф, Щ, Ъ – 10 очков.
+# Напишите программу, которая вычисляет стоимость введенного пользователем слова. 
+# Будем считать, что на вход подается только одно слово, которое содержит либо 
+# только английские, либо только русские буквы.
+# Ввод: ноутбук
+# Вывод: 12
+
+print('Program to find the value of the inputed word.')
+
+import os
+os.system('cls')
+
+english_dictionary = \
+    {
+        1: 'A, E, I, O, U, L, N, S, T, R',
+        2: 'D, G',
+        3: 'B, C, M, P',
+        4: 'F, H, V, W, Y',
+        5: 'K',
+        8: 'J, X',
+        10: 'Q, Z'
+    }
+
+russian_dictionary = \
+    {
+        1: 'А, В, Е, И, Н, О, Р, С, Т',
+        2: 'Д, К, Л, М, П, У',
+        3: 'Б, Г, Ё, Ь, Я',
+        4: 'Й, Ы',
+        5: 'Ж, З, Х, Ц, Ч',
+        8: 'Ш, Э, Ю',
+        10: 'Ф, Щ, Ъ'
+    }    
+
+
+language = input('Choose the language. For Russian press 1, for English press 2: ')
+word = input('Input the word in this language: ').upper()
+sum = 0
+
+if language == '1':
+    for i in word:
+        for k, v in russian_dictionary.items():
+            if i in v:
+                sum += k
+elif language == '2':
+    for i in word:
+        for k, v in english_dictionary.items():
+            if i in v:
+                sum += k     
+else: 
+    print('Wrong input. Please select English or Russian.')                            
+
+print(f'You have got {sum} points')
